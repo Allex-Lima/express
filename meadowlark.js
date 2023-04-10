0f053cc7cd666ee3754682e06e2569a9cd55cbd1
Alexandro <alexlmangabeira@gmail.com>
2023-04-10 18:08:46 -0300

    adicionando views e layouts

--- meadowlark.js
+++ meadowlark.js
@@ -1,44 +1,33 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+
 require('dotenv').config()
 
 const app = express()
 
 //configurar o view engine Handlebars
-app.engine('handlebars', expressHandlebars({
+app.engine('handlebars', expressHandlebars.engine({
 	defaultLayout: 'main',
 }))
-app.set('view engine', 'Handlebars')
+app.set('view engine', 'handlebars')
 
 const port = process.env.PORT | 3000
 
-app.get('/', (req, res) => {
-	res.type('text/plain')
-	res.send('Meadowlark Travel')
-})
+app.get('/', (req, res) => res.render('home'))
 
-app.get('/about*', (req, res) => {
-	res.type('text/plain')
-	res.send('About Meadowlark Travel')
+app.get('/about', (req, res) => {
+	res.render('about')
 })
-
-app.get('/about/contact', (req, res) => {
-	res.type('text/plain')
-	res.send('Contact')
-})
-
 app.use((req, res) => {
-	res.type('text/plain')
 	res.status(404)
-	res.send('404, - NOT FOUND.')
+	res.render('404, - NOT FOUND.')
 })
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
 	console.error(err.message)
-	res.type('text/plain')
-	res.status('500')
-	res.send('500 - server error')
+	res.status(500)
+	res.send('500')
 })
 
 app.listen(port, () => {
